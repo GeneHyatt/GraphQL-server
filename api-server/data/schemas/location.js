366ce29a00bf7e24b34cd6fc1059d538d221ce38
Gene Hyatt <gene.hyatt@gmail.com>
2023-01-17 11:39:03 -0500

    Adding framework for the frontend server.

--- api-server/data/schemas/location.js
+++ api-server/data/schemas/location.js
@@ -2,16 +2,29 @@ const Location = `
   type Query {
     states: [State]
     locations: [Location]
+    counties: [County]
+    locationDetails: [LocationDetails]
   }
+  
+  type County {
+    county: String
+  }
+
   type State {
       province: String
   }
   
   type Location {
+    locationID: String!
+    location: String
+    locationDetails: LocationDetails
+  }
+  
+  type LocationDetails {
       locationID: String!
       state: State
       county: String
-      location: String
+      address: String
       latitude: Float
       longitude: Float,
   }
---
+++ api-server/resolvers/index.js
@@ -0,0 +1,47 @@
+// TODO: move these to utils
+const uniqueValues = (array, propertyName) => {
+  return array.filter((e, i) => array.findIndex(a => a[propertyName] === e[propertyName]) === i);
+}
+
+
+// TODO: move the different specific resolvers to their own files.
+const resolvers = {
+  Query: {
+    sightings: (obj, args, context) => {
+      return context.sightings;
+    },
+    sighting: (obj, args, context) => {
+      const data = context.sightings.find(element => element.ID === args.id);
+      return Promise.resolve(data);
+    },
+    locations: (obj, args, context) => {
+      const locationArray = context.sightings.map((item) => {
+          return {
+            locationID: item.locationID,
+            location: item.location,
+            locationDetails: {
+              county: item.county,
+              latitude: item.latitude,
+              longitude: item.longitude
+            }
+          };
+        },
+      );
+      return Promise.resolve(locationArray);
+    },
+    counties: (obj, args, context) => {
+      console.log('countyArray', uniqueValues(context.sightings, 'country'))
+      return Promise.resolve(uniqueValues(context.sightings, 'country'));
+    },
+    states: (obj, args, context) => {
+      const stateObjArray = uniqueValues(context.sightings, 'state');
+      const provinceArray = uniqueValues(stateObjArray, 'province');
+      // console.log('stateObjArray', stateObjArray);
+      console.log('provinceArray', provinceArray);
+      // console.log('stateArray', uniqueValues(context.sightings, 'state'))
+      return Promise.resolve(stateObjArray);
+    },
+  },
+};
+
+export default resolvers;
